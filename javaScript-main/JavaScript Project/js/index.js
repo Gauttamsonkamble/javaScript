28c48c2c322aad80843280d9884b47864403a23a
Gauttam Sonkamble <46339795+Gauttamsonkamble@users.noreply.github.com>
2022-07-17 12:18:34 +0530

    Add files via upload
    logo updated

---
+++ javaScript-main/JavaScript Project/js/index.js
@@ -0,0 +1,9 @@
+function green() {
+    var v = document.getElementById("g1");
+    v.src = "images/p1.jpg";
+}
+
+function red() {
+    var v = document.getElementById("g1");
+    v.src = "images/p2.jpg";
+}
